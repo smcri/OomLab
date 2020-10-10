c6f0205c7b448e9848c00e1308f4a6ce21f39265
smcri <poashan1234@gmail.com>
2020-10-10 13:36:39 +0530

    First slider

--- Lab[06-10-2020]/main.java
+++ Lab[06-10-2020]/main.java
@@ -1,1 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
 
+class app extends JFrame
+{
+	app()
+	{
+		JPanel panel = new JPanel();
+		JSlider slider1 = new JSlider();
+		panel.add(slider1);
+		add(panel);
+		setSize(300,300);
+		show();
+	}
+}
+
+class main
+{
+	public static void main(String args[])
+	{
+		app a1 = new app();
+	}
+}
