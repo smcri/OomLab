c9adf47cbe81f2cc12a9ce892f3bedc266a1f435
smcri <poashan1234@gmail.com>
2020-10-10 14:36:44 +0530

    Added 4 required sliders

--- Lab[06-10-2020]/main.java
+++ Lab[06-10-2020]/main.java
@@ -7,8 +7,19 @@ class app extends JFrame
 	app()
 	{
 		JPanel panel = new JPanel();
-		JSlider slider1 = new JSlider();
-		panel.add(slider1);
+		JSlider slider[] = new JSlider[4];
+		for(int i = 0;i<4;i++)
+		{
+			slider[i] = new JSlider();
+			slider[i].setPaintTrack(true);
+			slider[i].setPaintTicks(true);
+			slider[i].setPaintLabels(true);
+
+			slider[i].setMajorTickSpacing(50); 
+        	slider[i].setMinorTickSpacing(5); 
+			panel.add(slider[i]);
+		}
+		
 		add(panel);
 		setSize(300,300);
 		show();
